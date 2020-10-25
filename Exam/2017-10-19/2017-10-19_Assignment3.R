e25882c7f899a2b1ec7d42a6e904e5b437a1a8e1
Christian von Koch <chrvo878@student.liu.se>
2020-10-25 16:51:09 +0100

    Progress on exam 2017

---
+++ Exam/2017-10-19/2017-10-19_Assignment3.R
@@ -0,0 +1,6 @@
+## Let f~GP(0,k(x,x')) a priori and let sigmaf^2 = 1. Simulate and plot 5 realizations from the prior distribution of f
+## over the grid xGrid=seq(-1,1,by=0.1) for the length scales l=0-2 and l=1 (in two separate figures). Compute:
+## i) Corr(f(0), f(0.1))
+## ii) Corr(f(0),f(0.5))
+## for the two length scales. Discuss the results and connect your discussion to the concept of smoothness of f.
+
