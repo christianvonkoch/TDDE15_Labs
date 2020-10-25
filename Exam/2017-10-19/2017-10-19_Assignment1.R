fac7292b4773a74db47bca66d88ae93df6ada7bf
Christian von Koch <chrvo878@student.liu.se>
2020-10-25 12:16:53 +0100

    Exam 2017

---
+++ Exam/2017-10-19/2017-10-19_Assignment1.R
@@ -0,0 +1,9 @@
+## Learn a Bayesian network (BN) from the Asia dataset that is included in the bnlearn
+## package. To load the data, run data("asia"). Learn both the structure and the
+## parameters. Use any learning algorithm and settings that you consider appropriate.
+## Identify a d-separation in the BN learned and show that it indeed corresponds to an
+## independence in the probability distribution represented by the BN. To do so, you may
+## want to use exact or approximate inference with the help of the bnlearn and gRain
+## packages. (2.5 p)
+
+
